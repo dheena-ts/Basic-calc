40a399b72dfa8208b8f05afd291c75be6baf8ce8
naveen3970 <naveen.c@mitrahsoft.in>
2026-03-17 11:45:49 +0530

    add the divide function

--- math.js
+++ math.js
@@ -7,9 +7,12 @@ function sub(a,b){
 function multiple(a,b){
     return a*b
 }
+function divide(a,b){
+    return a/b
+}
 
 console.log(add(10,20));
 console.log(sub(30,15));
 console.log(multiple(2,3));
-console.log(multiple(2,3));
+console.log(divide(10,5));
 
